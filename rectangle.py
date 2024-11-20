d265bfc1b7c63919298164b8edb1e29df0a7b47b
vitorgaboardi <51085335+vitorgaboardi@users.noreply.github.com>
2024-11-20 10:46:41 +0000

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,16 @@
+class Rectangle:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+
+  def set_width(self, width):
+    self.width = width
+
+  def set_height(self, height):
+    self.height = height
+
+  def get_area(self):
+    if self.width < 0 or self.height < 0:
+      return -1
+    else:
+      return self.width * self.height
